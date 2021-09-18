d504f3e5cc7a44a8ac7abfb5164db88a4aee7a17
datamesse <48945612+datamesse@users.noreply.github.com>
2021-09-18 21:01:16 +1000

    Begin work on project tiles layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,44 @@
-export default function Projects() {
-    return (
+import { logo_aws, logo_azure, logo_css, logo_excel, logo_html, logo_javascript, logo_mongodb, logo_nodejs,
+         logo_powerbi, logo_python, logo_react, logo_r, logo_tableau,logo_typescript, logo_visualstudio
+       } from '../data/assets';
 
-                <div className='projects'>
-                </div>
+const portfolio = [
+    {
+        title: 'data_messe Portfolio',
+        description: 'My portfolio site which I designed and built from scratch using React.js and GitHub Pages package.',
+        stack: { logo_react },
+        siteURL: 'https://datamesse.github.io/',
+        codeURL: 'https://github.com/datamesse/datamesse.github.io/'
+    },
+    {
+        title: 'Test',
+        description: 'My portfolio site which I designed and built from scratch using React.js and GitHub Pages package.',
+        stack: { logo_react },
+        siteURL: 'https://datamesse.github.io/',
+        codeURL: 'https://github.com/datamesse/datamesse.github.io/'
+    }
+]
 
-    )
+function Projects() {
+  return (
+    <div className='projects'>
+        <center>
+        <h2>Projects</h2>
+            <div className='project-set'>
+            {
+                portfolio.map(function(port, index){
+                    return (
+                        <div key={index} className='project-tile'> 
+                            <h3>{port.title}</h3>
+                            <p>{port.description}</p>
+                        </div>
+                    )
+                })
+            }
+            </div>
+        </center>
+    </div>
+  )
 }
+
+export default Projects
--- src/data/data.js
+++ src/data/data.js
@@ -4,8 +4,4 @@ const githubURL = 'https://github.com/datamesse'
 const twitterURL = 'https://twitter.com/data_messe/'
 const tableauURL = 'https://public.tableau.com/profile/data.messe#!/'
 
-const projects = [
-    
-]
-
 export { displayName, bannerDescription, githubURL, twitterURL, tableauURL }
