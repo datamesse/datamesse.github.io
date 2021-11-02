88dea118eb79a8a829cf15376c617960051cacd3
datamesse <48945612+datamesse@users.noreply.github.com>
2021-11-02 22:42:26 +1100

    Add Power BI November project

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -5,8 +5,18 @@ import { logo_aws, logo_azure, logo_css, logo_excel, logo_html, logo_javascript,
  } from '../data/assets';
 import img_portfoliowebsitereact from '../assets-portfolio/img-2021-09-portfolio-website-react.png';
 import img_singaporerentpowerbi from '../assets-portfolio/img-2021-10-power-bi-quarterly-singapore-rental-prices-by-currency.png';
+import img_followthesunpowerbi from  '../assets-portfolio/img-2021-11-power-bi-follow-the-sun-customer-support.png';
 
 const portfolio = [
+   {
+      title: 'Follow the sun customer support',
+      description: 'Power BI report showing global teamwork and performance for customer service',
+      stamp: '2021-11',
+      stack: logo_powerbi,
+      photo: img_followthesunpowerbi,
+      siteURL: 'https://app.powerbi.com/view?r=eyJrIjoiZTc5MzQ4OWYtMTg5OS00NmQ3LWExNzgtNDE5ZmE5ZDRlOWQ4IiwidCI6IjM5MTA5NGVmLTAxN2YtNGUwMy1hM2NjLTQ1OGQ3N2I1YjBjZSJ9',
+      codeURL: 'https://github.com/datamesse/data-visualisation-datasets/tree/main/Support%20ticket%20updates'
+   },
    {
       title: 'Singapore rental prices',
       description: 'Power BI report showing trends of Singapore rent by quarter, major currency, and flat type',
