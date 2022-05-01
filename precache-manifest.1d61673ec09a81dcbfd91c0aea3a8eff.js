b1689dc27edeb710f5712acc81123c8edac17bd8
datamesse <datamesse@users.noreply.github.com>
2022-05-01 16:23:08 +1000

    Updates

---
+++ precache-manifest.1d61673ec09a81dcbfd91c0aea3a8eff.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d22ec7c09e64970f934",
+    "url": "/static/css/main.311f4cb2.chunk.css"
+  },
+  {
+    "revision": "5d22ec7c09e64970f934",
+    "url": "/static/js/main.5d22ec7c.chunk.js"
+  },
+  {
+    "revision": "37154309d4c7298ad89f",
+    "url": "/static/js/1.37154309.chunk.js"
+  },
+  {
+    "revision": "51c9577bfe88015a007c",
+    "url": "/static/js/2.51c9577b.chunk.js"
+  },
+  {
+    "revision": "c817288b36792fb65901",
+    "url": "/static/js/runtime~main.c817288b.js"
+  },
+  {
+    "revision": "b90dc4f8343dbdd2563ac6b5cf5c9a93",
+    "url": "/static/media/bannervideo-bluerectangles2.b90dc4f8.mp4"
+  },
+  {
+    "revision": "fdff4014b22f67787c176c6aa52890a7",
+    "url": "/static/media/logo-aws.fdff4014.svg"
+  },
+  {
+    "revision": "cc412b1eff61906c6d83fd9ccca6751c",
+    "url": "/static/media/logo-azure.cc412b1e.svg"
+  },
+  {
+    "revision": "15678ffe04860272db359a247ec7fc08",
+    "url": "/static/media/logo-css.15678ffe.svg"
+  },
+  {
+    "revision": "be1669c6f8d23941383ea8454dfb43f5",
+    "url": "/static/media/logo-excel.be1669c6.svg"
+  },
+  {
+    "revision": "c38b6985e809168a4f957c6bc978d08a",
+    "url": "/static/media/logo-github.c38b6985.svg"
+  },
+  {
+    "revision": "d84fde66a018a9c27722c99989c90d94",
+    "url": "/static/media/logo-html.d84fde66.svg"
+  },
+  {
+    "revision": "f935b988635bb10498a83f5f012ce214",
+    "url": "/static/media/logo-javascript.f935b988.svg"
+  },
+  {
+    "revision": "5a996c66cbcea7fbbe3ce0fcfac739b7",
+    "url": "/static/media/logo-linkedin.5a996c66.svg"
+  },
+  {
+    "revision": "c2a864c96dbfc449ec45dd7ac4438628",
+    "url": "/static/media/logo-mongodb.c2a864c9.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/static/media/logo-nodejs.a1231528.svg"
+  },
+  {
+    "revision": "c70d8b4376bbadf9f8d14fc0ff0b8ac2",
+    "url": "/static/media/logo-powerbi.c70d8b43.svg"
+  },
+  {
+    "revision": "9ecfb67266abc5a684cdce0635f4dc3c",
+    "url": "/static/media/logo-python.9ecfb672.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo-react.06e73328.svg"
+  },
+  {
+    "revision": "ef95b59e766c1d5d3e7c5a461320836b",
+    "url": "/static/media/logo-rstudio.ef95b59e.svg"
+  },
+  {
+    "revision": "7afc51d375623dfb72a7857c74e7e80d",
+    "url": "/static/media/logo-sqlserver.7afc51d3.svg"
+  },
+  {
+    "revision": "148cae2ec843ecef83caf4606d9d3c88",
+    "url": "/static/media/logo-tableau.148cae2e.svg"
+  },
+  {
+    "revision": "c86b433adb9ba5e02c22235a07dbc87c",
+    "url": "/static/media/logo-twitter.c86b433a.svg"
+  },
+  {
+    "revision": "0cdb301b441ff92462b5cc6ade47e437",
+    "url": "/static/media/logo-typescript.0cdb301b.svg"
+  },
+  {
+    "revision": "e60fcd0974402a2aaf59f3835776c5ea",
+    "url": "/static/media/logo-visualstudiocode.e60fcd09.svg"
+  },
+  {
+    "revision": "ee528d4fd418acd30f317e6a54404cd6",
+    "url": "/static/media/logo-youtube.ee528d4f.svg"
+  },
+  {
+    "revision": "b3bb898867707cffb2283cc7634f730b",
+    "url": "/static/media/logoblue-github.b3bb8988.svg"
+  },
+  {
+    "revision": "56e8d75c59993de31791d04273f47ac7",
+    "url": "/static/media/logoblue-linkedin.56e8d75c.svg"
+  },
+  {
+    "revision": "fbc8a721b6aa27e254a424e5ad74d7f2",
+    "url": "/static/media/logoblue-powerbi.fbc8a721.svg"
+  },
+  {
+    "revision": "b8ed413658d87f3f5ab2c599cacc0dd1",
+    "url": "/static/media/logoblue-tableau.b8ed4136.svg"
+  },
+  {
+    "revision": "dfac15a2c3718dc5003dca42429b2c0a",
+    "url": "/static/media/logoblue-twitter.dfac15a2.svg"
+  },
+  {
+    "revision": "90400f1e0a1ae6fa4908a19b7204e54f",
+    "url": "/static/media/logoblue-youtube.90400f1e.svg"
+  },
+  {
+    "revision": "28f6f8d006925c343c743700edb59b11",
+    "url": "/static/media/img-2021-09-portfolio-website-react.28f6f8d0.png"
+  },
+  {
+    "revision": "5e5019ffa3ede722e0114e373b477d21",
+    "url": "/static/media/img-2021-10-power-bi-quarterly-singapore-rental-prices-by-currency.5e5019ff.png"
+  },
+  {
+    "revision": "5646c46ee62f3d09e21badeefe560999",
+    "url": "/static/media/img-2021-11-power-bi-follow-the-sun-customer-support.5646c46e.png"
+  },
+  {
+    "revision": "29c8278767a457c0a4d60a1e8e27ca5e",
+    "url": "/static/media/img-2022-01-sql-server-integration-services-international-marketplace.29c82787.png"
+  },
+  {
+    "revision": "51bf9a11f8d2bdd7679daa149a5ec264",
+    "url": "/static/media/img-2022-04-power-bi-international-marketplace-python-deneb.51bf9a11.png"
+  },
+  {
+    "revision": "4f95bd9246282c2756b06e2d6756cd70",
+    "url": "/index.html"
+  }
+];
