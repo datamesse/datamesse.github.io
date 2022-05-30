45c4cefe7966770ee1dc5e804aa43d37d0cb2867
datamesse <datamesse@users.noreply.github.com>
2022-05-30 21:50:44 +1000

    Updating asset files

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import '../App.css';
 import MenuDesktop from './MenuDesktop';
 import MenuMobile from './MenuMobile';
-import { icon_menuburger } from '../data/assets'
-import { displayName, ascii2, ascii3, ascii4 } from '../data/data'
+import { icon_menuburger, letter_01, letter_08, letter_09, letter_10, letter_11, letter_13, letter_14, letter_18, letter_21 } from '../data/assets';
+
+/*
+import { displayName, ascii2, ascii3, ascii4 } from '../data/data';
 
 var headings = [displayName, ascii2+ascii3+ascii4];
 var i = 0;
@@ -14,6 +16,9 @@ var intervalId = setInterval(function() {
   }
 }, 1000)
 
+            <div className="sitename" id="title"></div>
+*/
+
 function Header() {
 
 const [isOpen, setIsOpen] = useState(false)
@@ -21,7 +26,19 @@ const [isOpen, setIsOpen] = useState(false)
     return (
         <>
             <div className="header">
-            <div className="sitename" id="title"></div>
+            <div className="sitename" id="title">
+                <img className="site-letter" src={ letter_10 } />
+                <img className="site-letter" src={ letter_21 } />
+                <img className="site-letter" src={ letter_14 } />
+                <img className="site-letter" src={ letter_13 } />
+                <img className="site-letter" src={ letter_01 } />
+                <img className="site-letter" src={ letter_11 } />
+                <img className="site-letter" src={ letter_21 } />
+                <img className="site-letter" src={ letter_08 } />
+                <img className="site-letter" src={ letter_01 } />
+                <img className="site-letter" src={ letter_18 } />
+                <img className="site-letter" src={ letter_09 } />
+            </div>
                 <div className="menu">
                     <div className="menu-desktop">
                         <MenuDesktop />
