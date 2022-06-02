2223d17e23326b90d864a0fbc41d9d12d75584b5
datamesse <datamesse@users.noreply.github.com>
2022-06-03 08:14:53 +1000

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ function About() {
                         <img className='about-avatar' src={ avatar } />                    
                         <h1>data messe</h1>
                     </div>
-                    <p>Hi, I'm a data analysis and visualisation specialist with over 8 years of customer service and technical troubleshooting experience.</p>
+                    <p>Hi, I'm a data analysis and visualisation enthusiast with over 8 years of customer service and technical troubleshooting experience.</p>
                     <p>My goal is to help great clients solve problems and make the best decisions they can using data.</p>
                     <div className='about-quote'>
                         <a href="https://en.wikipedia.org/wiki/Messe" target='_blank'>"messe" is a German word meaning trade fair;  - Wikipedia</a>
