0284aeea9b7ccfa4369fe86749de284486283a14
datamesse <datamesse@users.noreply.github.com>
2022-06-04 14:05:15 +1000

    Updating CSS for blog preview component

--- src/components/BlogPreview.js
+++ src/components/BlogPreview.js
@@ -2,17 +2,59 @@ import React from 'react';
 import Separator from './Separator';
 import '../App.css';
 import { Link } from 'react-router-dom';
+import { assetURL } from '../data/data';
+import Posts from '../data/posts.json';
+import ReactMarkdown from 'react-markdown';
 
-function BlogPreview() {
+export default function BlogPreview() {
+
+    const PostsPreview = Posts.slice(0,3);
+
+    const countAllPosts = Posts.length;
+
+    const excerptList = PostsPreview.map(post => {
+        return post.content.split(" ").slice(0, 30).join(" ") + ' . . . '
+    })
+
     return (
         <div className='blog-preview' id='blog'>
             <Separator />
             <label className='section-title'>Blog</label>
-            <Link to='/blog' target='_blank'>
-                Click here to preview all blog posts
+
+
+            <div className='blog-set-preview'>
+                    {
+                        PostsPreview.length &&
+                            PostsPreview.map((post, i) => {
+                                return (
+                                    <div key={i} className='blog-tile-preview'>
+                                        <div className='blog-tile-header-preview'>
+                                            <div className='blog-icon-title-preview'>
+                                                <span className='blogstamp-overlay'>
+                                                    <img src= {`${ assetURL }${post.tag}.svg`} alt=''/>
+                                                </span>
+                                                <Link to={`/post/${post.id}`}>{post.title}</Link>
+                                            </div>
+                                            <div className='blog-meta-preview'>
+                                                <small>{post.date}</small>
+                                            </div>
+                                        </div>
+                                        <ReactMarkdown className='blog-desc-preview' escapeHtml={false}>{excerptList[i]}</ReactMarkdown>
+                                        <div className='blog-meta-preview'>
+                                            <small>
+                                                <Link to={`/post/${post.id}`}>Read more...</Link>
+                                            </small>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                    }
+                    </div>
+
+
+            <Link to='/blog'>
+                Click here to see all { countAllPosts } blog posts
             </Link>
         </div>
     );
 }
-
-export default BlogPreview;
