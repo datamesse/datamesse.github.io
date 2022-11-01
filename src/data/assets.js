4966daa23435bf0f11b91e91bbac7b1ebfbded33
Ⓓⓐⓣⓐ Ⓜⓔⓢⓢⓔ <48945612+datamesse@users.noreply.github.com>
2022-11-01 20:13:05 +1100

    Upload colour changed menu links

--- src/data/assets.js
+++ src/data/assets.js
@@ -8,15 +8,15 @@ import image_onthewaypana from '../assets-theme/storyset-on-the-way-pana.svg';
 
 /* Icons */
 import icon_calendarlinespen from '../assets-theme/flaticon-calendar-lines-pen.svg';
-import icon_charthistogram from '../assets-theme/flaticon-chart-histogram.svg';
+import icon_charthistogram from '../assets-theme/flaticon-chart-histogram-white.svg';
 import icon_crosscircle from '../assets-theme/flaticon-cross-circle.svg';
-import icon_diploma from '../assets-theme/flaticon-diploma.svg';
-import icon_edit from '../assets-theme/flaticon-edit.svg';
+import icon_diploma from '../assets-theme/flaticon-diploma-white.svg';
+import icon_edit from '../assets-theme/flaticon-edit-white.svg';
 import icon_globe from '../assets-theme/flaticon-globe.svg';
 import icon_keyboard from '../assets-theme/flaticon-keyboard.svg';
 import icon_magicwand from '../assets-theme/flaticon-magic-wand.svg';
 import icon_menuburger from '../assets-theme/flaticon-menu-burger.svg';
-import icon_paperplane from '../assets-theme/flaticon-paper-plane.svg';
+import icon_paperplane from '../assets-theme/flaticon-paper-plane-white.svg';
 
 /* Logos */
 import logo_aws from '../assets-theme/logo-aws.svg'
