5f40df62b86f07240e645401229cb5fde6ea11d5
Ⓓⓐⓣⓐ Ⓜⓔⓢⓢⓔ <48945612+datamesse@users.noreply.github.com>
2023-07-26 23:50:17 +1000

    Redesigned CSS for bottom menu buttons
    Realised that there's no sense in scaling the text labels for the buttons at the bottom of the menu on cursor hover. So I changed the component by separating out the label from the scaled <a> tags, and updated the CSS for the redesigned behaviour.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,34 +12,51 @@ function Menu() {
 
     return (
 
-        <div className='menu-buttons-container'>
+        <div className='menu-container'>
 
-            <Link to="/portfolio" onClick={start}>
-                <div className='menu-button-border menu-button-border-square'></div>
-                <div className='menu-button-highlight menu-button-highlight-square menu-button-highlight-blue'></div>
-                <div className='menu-button-gradient menu-button-gradient-square menu-button-gradient-blue'></div>
-                <img className='menu-button-icon' src={ icon_charthistogram } />
-                <div className='menu-button-light'></div>
-                <p>PORTFOLIO</p>
-            </Link>
+            <div className='menu-buttons-container'>
 
-            <Link to="/blog" onClick={start}>
-                <div className='menu-button-border menu-button-border-circle'></div>
-                <div className='menu-button-highlight menu-button-highlight-circle menu-button-highlight-teal'></div>
-                <div className='menu-button-gradient menu-button-gradient-circle menu-button-gradient-teal'></div>
-                <img className='menu-button-icon' src={ icon_edit } />
-                <div className='menu-button-light'></div>
-                <p>BLOG</p>
-            </Link>
+                <Link to="/portfolio" onClick={start}>
+                    <div className='menu-button-border menu-button-border-square'></div>
+                    <div className='menu-button-highlight menu-button-highlight-square menu-button-highlight-blue'></div>
+                    <div className='menu-button-gradient menu-button-gradient-square menu-button-gradient-blue'></div>
+                    <img className='menu-button-icon' src={ icon_charthistogram } />
+                </Link>
 
-            <Link to="/" onClick={start}>
-                <div className='menu-button-border menu-button-border-square'></div>
-                <div className='menu-button-highlight menu-button-highlight-square menu-button-highlight-pink'></div>
-                <div className='menu-button-gradient menu-button-gradient-square menu-button-gradient-pink'></div>
-                <img className='menu-button-icon' src={ icon_modeportrait } />
-                <div className='menu-button-light'></div>
-                <p>PROFILE</p>
-            </Link>
+                <Link to="/blog" onClick={start}>
+                    <div className='menu-button-border menu-button-border-circle'></div>
+                    <div className='menu-button-highlight menu-button-highlight-circle menu-button-highlight-teal'></div>
+                    <div className='menu-button-gradient menu-button-gradient-circle menu-button-gradient-teal'></div>
+                    <img className='menu-button-icon' src={ icon_edit } />
+                </Link>
+
+                <Link to="/" onClick={start}>
+                    <div className='menu-button-border menu-button-border-square'></div>
+                    <div className='menu-button-highlight menu-button-highlight-square menu-button-highlight-pink'></div>
+                    <div className='menu-button-gradient menu-button-gradient-square menu-button-gradient-pink'></div>
+                    <img className='menu-button-icon' src={ icon_modeportrait } />
+                </Link>
+
+            </div>
+
+            <div className='menu-labels-container'>
+
+                <div className='menu-label'>
+                    <div className='menu-label-light'></div>
+                    <p>PORTFOLIO</p>
+                </div>
+
+                <div className='menu-label'>
+                    <div className='menu-label-light'></div>
+                    <p>BLOG</p>
+                </div>
+
+                <div className='menu-label'>
+                    <div className='menu-label-light'></div>
+                    <p>PROFILE</p>
+                </div>
+
+            </div>
 
         </div>
 
