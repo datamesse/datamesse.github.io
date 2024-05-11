4e52cb0dfffb842099046b34f568bcb284812859
Ⓓⓐⓣⓐ Ⓜⓔⓢⓢⓔ <48945612+datamesse@users.noreply.github.com>
2024-05-11 12:56:41 +1000

    Updates

---
+++ precache-manifest.563519da9bbbffcc0309cb90151522d5.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "19a6cc51f4dfb3d2523e",
+    "url": "/static/css/main.04b80333.chunk.css"
+  },
+  {
+    "revision": "19a6cc51f4dfb3d2523e",
+    "url": "/static/js/main.19a6cc51.chunk.js"
+  },
+  {
+    "revision": "4927455309981692546f",
+    "url": "/static/js/1.49274553.chunk.js"
+  },
+  {
+    "revision": "8ea896f2677a51df8b11",
+    "url": "/static/js/2.8ea896f2.chunk.js"
+  },
+  {
+    "revision": "d8b21ae57e91e2f65ec3",
+    "url": "/static/js/runtime~main.d8b21ae5.js"
+  },
+  {
+    "revision": "2a0b694aa164f4639de93ee9fa38d011",
+    "url": "/static/media/avatar-vertical-transparent-bg.2a0b694a.png"
+  },
+  {
+    "revision": "bad7d04c729cc981237dc940512552db",
+    "url": "/static/media/flaticon-chart-histogram-white-glow.bad7d04c.svg"
+  },
+  {
+    "revision": "04bc70bb4c2f363b28fa41475e3be70a",
+    "url": "/static/media/flaticon-circle-right-orange.04bc70bb.svg"
+  },
+  {
+    "revision": "07969b468b656ee0e1a9f18cb1f5a755",
+    "url": "/static/media/flaticon-edit-white-glow.07969b46.svg"
+  },
+  {
+    "revision": "30f02fe025a3718e69baead791a5b6a9",
+    "url": "/static/media/flaticon-mode-portrait-white-glow.30f02fe0.svg"
+  },
+  {
+    "revision": "fdff4014b22f67787c176c6aa52890a7",
+    "url": "/static/media/logo-aws.fdff4014.svg"
+  },
+  {
+    "revision": "cc412b1eff61906c6d83fd9ccca6751c",
+    "url": "/static/media/logo-azure.cc412b1e.svg"
+  },
+  {
+    "revision": "15678ffe04860272db359a247ec7fc08",
+    "url": "/static/media/logo-css.15678ffe.svg"
+  },
+  {
+    "revision": "be1669c6f8d23941383ea8454dfb43f5",
+    "url": "/static/media/logo-excel.be1669c6.svg"
+  },
+  {
+    "revision": "c38b6985e809168a4f957c6bc978d08a",
+    "url": "/static/media/logo-github.c38b6985.svg"
+  },
+  {
+    "revision": "76679d48aa0e13ca38a6a9def6464aa3",
+    "url": "/static/media/logo-neo4j.76679d48.svg"
+  },
+  {
+    "revision": "c70d8b4376bbadf9f8d14fc0ff0b8ac2",
+    "url": "/static/media/logo-powerbi.c70d8b43.svg"
+  },
+  {
+    "revision": "9ecfb67266abc5a684cdce0635f4dc3c",
+    "url": "/static/media/logo-python.9ecfb672.svg"
+  },
+  {
+    "revision": "7afc51d375623dfb72a7857c74e7e80d",
+    "url": "/static/media/logo-sqlserver.7afc51d3.svg"
+  },
+  {
+    "revision": "148cae2ec843ecef83caf4606d9d3c88",
+    "url": "/static/media/logo-tableau.148cae2e.svg"
+  },
+  {
+    "revision": "15692e0565847af35414895193e85d8b",
+    "url": "/static/media/music-evgeny-bardyuzha-good-mood-for-no-reason.15692e05.mp3"
+  },
+  {
+    "revision": "d8518d8031e3dec251d27f234a8c6a62",
+    "url": "/static/media/music-evgeny-bardyuzha-time-flows.d8518d80.mp3"
+  },
+  {
+    "revision": "fde51714ff254a1ab908c2cceefc0f69",
+    "url": "/static/media/music-evgeny-bardyuzha-small-discoveries.fde51714.mp3"
+  },
+  {
+    "revision": "a62086ef91ab5b5dc036710f9c3b3e34",
+    "url": "/static/media/mixkit-high-tech-bleep-confirmation-2520.a62086ef.wav"
+  },
+  {
+    "revision": "ae2c2c4d0cae54367043a33313619ae6",
+    "url": "/static/media/mixkit-technology-notification-3123.ae2c2c4d.wav"
+  },
+  {
+    "revision": "d02c89857f00459d18b237ac6a624072",
+    "url": "/static/media/img-2023-07-portfolio-website-react-v4.d02c8985.gif"
+  },
+  {
+    "revision": "944dc5d6882fde310a4c02063d2005e8",
+    "url": "/static/media/img-2021-10-power-bi-quarterly-singapore-rental-prices-by-currency.944dc5d6.gif"
+  },
+  {
+    "revision": "5d3b31d545b68b2444709a9b7649ba25",
+    "url": "/static/media/img-2021-11-power-bi-follow-the-sun-customer-support.5d3b31d5.gif"
+  },
+  {
+    "revision": "7971d43c7eeff01147d527db8e890396",
+    "url": "/static/media/img-2022-01-sql-server-integration-services-international-marketplace-trimmed.7971d43c.png"
+  },
+  {
+    "revision": "fab788807da2ae6ca1ed7d8ab8a438e9",
+    "url": "/static/media/img-2022-04-power-bi-international-marketplace-python-deneb.fab78880.gif"
+  },
+  {
+    "revision": "785039de8a646390f2cc43986ef742dd",
+    "url": "/static/media/img-2022-06-excel-customer-support-agent-performance.785039de.gif"
+  },
+  {
+    "revision": "30e65a6bbea09aca0d12cd9a2e476822",
+    "url": "/static/media/img-2022-08-satellite-launch-overview.30e65a6b.gif"
+  },
+  {
+    "revision": "3adfbfdaa0a4532d9154687403a26df3",
+    "url": "/static/media/img-2022-10-excel-international-marketplace-profit-forecast.3adfbfda.gif"
+  },
+  {
+    "revision": "f42b54e48e9c0f124f0af7a76ef9b6fe",
+    "url": "/static/media/img-2022-11-power-bi-appsource-deneb-maps.f42b54e4.gif"
+  },
+  {
+    "revision": "6f7a2c0bf4364cb971a6a2621485eabd",
+    "url": "/index.html"
+  }
+];
