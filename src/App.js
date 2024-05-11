7da81856c18ab65a68c7cdf2030f91408c5de8f1
Ⓓⓐⓣⓐ Ⓜⓔⓢⓢⓔ <48945612+datamesse@users.noreply.github.com>
2024-05-11 14:11:21 +1000

    Reverting loading screen
    Reversing addition of loading screen, as I couldn't get it to work the way I needed.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Home from './components/Home';
-import preloadAssets from './data/assets';
-import Loading from './Loading';
+import React from "react";
+import Home from "./components/Home";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    const loadAssets = () => {
-      preloadAssets();
-      let currentProgress = 0;
-      const interval = setInterval(() => {
-        currentProgress += 10; // Increase progress by 10% in each interval
-        setProgress(currentProgress);
-        if (currentProgress >= 100) {
-          clearInterval(interval);
-          setLoading(false); // Hide loading screen when progress reaches 100%
-        }
-      }, 200); // Adjust the interval timing as needed
-    };
-
-    loadAssets();
-  }, []);
-
-  return (
-    <div>
-      <Loading loading={loading} progress={progress} />
-      {!loading && <FadeOut><Home /></FadeOut>}
-    </div>
-  );
-};
-
-const FadeOut = ({ children }) => {
-  const [fadeOut, setFadeOut] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setFadeOut(true);
-    }, 1000);
-  }, []);
-
-  return (
-    <div className={`fade-out ${fadeOut ? 'fade-out-active' : ''}`}>
-      {children}
-    </div>
-  );
+  return <Home />
 };
 
 export default App;
